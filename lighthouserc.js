ac613b1338cf0121f1a251de95087c0176c7ef08
Ruben Maloba <rubennkulu09@gmail.com>
2026-02-13 17:23:53 -0500

    chore(lhci): speed up runs and use preview server

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,9 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: "npm run start",
+      startServerCommand:
+        "npm run preview -- --host 127.0.0.1 --port 3000 --strictPort",
+      numberOfRuns: 1,
       url: [
         "http://localhost:3000/",
         "http://localhost:3000/contact",
